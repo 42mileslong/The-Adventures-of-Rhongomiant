6130dec8be8cae490ae42383552448b770e4503e
Rafi Long <rafilong42@gmail.com>
2014-05-13 11:21:57 -0700

    Added world files

--- src/world/World.java
+++ src/world/World.java
@@ -5,9 +5,19 @@ package world;
  * @author Rafi Long
  */
 public class World {
-
+    /**
+     * The world
+     */
     public static World world = new World();
 
+    /**
+     * The world loaded in chunks
+     */
+    public Chunk[] chunks = new Chunk[10];
+
+    /**
+     * World constructor
+     */
     public World() {
 
     }
