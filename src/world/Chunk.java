a6172f1306e12b2dd273964c6aa7e37dee9296a3
Rafi Long <rafilong42@gmail.com>
2014-05-13 11:08:00 -0700

    Added basic chunk methods

--- src/world/Chunk.java
+++ src/world/Chunk.java
@@ -5,14 +5,28 @@ package world;
  * @author Rafi Long
  */
 public class Chunk {
-    public int[][] chunk = new int[16][32];
+    public Type[][] chunk = new Type[32][32];
 
-    public Chunk() {
+    enum Type {AIR, DIRT, WOOD, STONE, GLASS}
 
+    public Chunk(int ground) {
+        //sets the ground level
+        for (int y = 0; y >= 32; y++) {
+            for (int x = 0; x < 32; x++) {
+                if (y < ground) {
+                    chunk[x][y] = Type.DIRT;
+                } else {
+                    chunk[x][y] = Type.AIR;
+                }
+            }
+        }
     }
 
-    public Chunk(int[][] grid) {
-        chunk = grid;
+    public void setBlock(int x1, int y1, int x2, int y2, Type type) {
+        for (int x = x1; x < x2; x++) {
+            for (int y = y1; y < y2; y++) {
+                chunk[x][y] = type;
+            }
+        }
     }
-
 }
