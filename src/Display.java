749ef106b4b47ca0845c1ef5802e672ec5aee1ef
Rafi Long <rafilong42@gmail.com>
2014-05-15 10:41:33 -0700

    Finished display

--- src/Display.java
+++ src/Display.java
@@ -51,18 +51,24 @@ public class Display extends JComponent {
     /**
      * Paints the board
      */
-    public void paintComponent() {
-
+    public void paint(Graphics g) {
+        super.paint(g);
+        drawBackground(g);
     }
 
     /**
      * Paints the grid
      * @param g Graphics argument
      */
-    public void drawGrid(Graphics g) {
-        for (int x = 0; x < 120; x++) {
-            for (int y = 0; y < 80; y++) {
-
+    public void drawBackground(Graphics g) {
+        for (int x = 0; x < 60; x++) {
+            for (int y = 0; y < 40; y++) {
+                for (int r = 0; r < 8; r++) {
+                    for (int c = 0; c < 8; c++) {
+                        g.setColor(Main.world.chunks[0].chunk[x][y].colors[r][c]);
+                        g.fillRect(2 * x, 2 * (y + 1), 2, 2);
+                    }
+                }
             }
         }
     }
